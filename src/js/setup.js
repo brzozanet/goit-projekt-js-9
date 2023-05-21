8581045b8081278c517094ce315c96adf69e17e4
Paweł Brzozowski <pawel@brzoza.net>
2023-05-21 18:15:19 +0200

    New API key

--- src/js/setup.js
+++ src/js/setup.js
@@ -1,4 +1,4 @@
-export const API_KEY = "2848be60a00d93888ab932a5bd427b76";
+export const API_KEY = "d6196550a4e6b0a064e85de7d6547ab4";
 export const IMG_URL = "https://image.tmdb.org/t/p/w500";
 export const URL = "https://api.themoviedb.org/3/";
 export const LANGUAGE = "en-US";
