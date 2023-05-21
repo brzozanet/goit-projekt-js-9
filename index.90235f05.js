1c95941d7e7c0954712f175130a3a4b728897ed2
brzozanet <pawel@brzoza.net>
2023-05-21 15:57:42 +0000

    Deploying to gh-pages from @ brzozanet/goit-projekt-js-9@1a713c19cbfd6c543c5de3f1e14a0dc145c6ba15 🚀

--- index.90235f05.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e={method:"GET",headers:{accept:"application/json",Authorization:"Bearer b7287b7e99d21f8e33dbc27c867ef458"}},o=document.querySelector("#movies-container-index");(async()=>{const a=await(async()=>{try{const o=await fetch("https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",e);return(await o.json()).results}catch(e){console.error(e)}})();console.log(a),a.forEach((e=>{o.innerHTML+=`<div class="movie-placeholder">${e.original_title}</div>`}))})()})();
-//# sourceMappingURL=index.90235f05.js.map
