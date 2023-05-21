ae36d97e122646a132d97eeaa826368661d5aed3
Paweł Brzozowski <pawel@brzoza.net>
2023-05-21 20:37:15 +0200

    Minor fixes

--- src/js/setup.js
+++ src/js/setup.js
@@ -1,4 +1,4 @@
-export const API_KEY = "d6196550a4e6b0a064e85de7d6547ab4";
+export const API_KEY = "bf096adfc02b1d9503cca39671f19618";
 export const IMG_URL = "https://image.tmdb.org/t/p/w500";
 export const URL = "https://api.themoviedb.org/3/";
 export const LANGUAGE = "en-US";
