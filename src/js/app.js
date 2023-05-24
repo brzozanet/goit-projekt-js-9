9d929d7fe3a41ddab9f43ec97285f750dd123f83
Mateusz Lis <mateusz.a.lis@gmail.com>
2023-05-24 12:30:05 +0200

    MODAL - do NOT work & little fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
 import "../sass/main.scss";
 import { fetchMostPopular } from "./most-popular";
+import { showSpinner, hideSpinner } from "./loading-spinner";
 // import { showModal } from "./modal";
 // showModal();
 fetchMostPopular();
