035aed8880c3ff7a86a7f340555dad0638446618
Mateusz Lis <mateusz.a.lis@gmail.com>
2023-05-25 14:44:51 +0200

    feature/modal BIG UPDATE
    ADDED
    1. modal
    2. downloading infos to modal
    3. correcting the code
    4. effects

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,4 @@
 import "../sass/main.scss";
 import { fetchMostPopular } from "./most-popular";
+import { showModal, closeModal, toggleWatched, toggleQueue } from "./modal";
 import { showSpinner, hideSpinner } from "./loading-spinner";
-import { showModal } from "./modal";
-
-fetchMostPopular();
-showModal();
--- src/js/modal.js
+++ src/js/modal.js
@@ -1,16 +1,63 @@
-import "../sass/main.scss";
+// export const showModal = () => {
+//   const refs = {
+//     openModalBtn: document.querySelector("[data-modal-open]"),
+//     closeModalBtn: document.querySelector("[data-modal-close]"),
+//     modal: document.querySelector("[data-modal]"),
+//   };
 
-export const showModal = () => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-  };
+//   function toggleModal() {
+//     refs.modal.classList.toggle("is-hidden");
+//   }
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+//   refs.openModalBtn.addEventListener("click", toggleModal);
+//   refs.closeModalBtn.addEventListener("click", toggleModal);
+// };
+export { showModal, closeModal, toggleWatched, toggleQueue };
+window.addEventListener("load", () => {
+  document
+    .querySelector("#modal__button-watched")
+    .addEventListener("click", toggleWatched);
+  document
+    .querySelector("#modal__button-queue")
+    .addEventListener("click", toggleQueue);
+  document.addEventListener("keyup", e => {
+    if (e.key === "Escape") closeModal();
+  });
+  document.addEventListener("click", e => {
+    if (!e.target.classList.contains("modal")) closeModal();
+  });
+  document.querySelector(".modal").addEventListener("click", e => {
+    e.stopPropagation();
+  });
+});
+function toggleWatched() {
+  document
+    .querySelector("#modal__button-watched")
+    .classList.toggle("button-active");
+  if (
+    document
+      .querySelector("#modal__button-watched")
+      .classList.contains("button-active")
+  ) {
+    document.querySelector("#modal__button-watched").textContent =
+      "Remove from watched";
+  } else {
+    document.querySelector("#modal__button-watched").textContent =
+      "Add to watched";
   }
-
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-};
+}
+function toggleQueue() {
+  document
+    .querySelector("#modal__button-queue")
+    .classList.toggle("button-active");
+  if (
+    document
+      .querySelector("#modal__button-queue")
+      .classList.contains("button-active")
+  ) {
+    document.querySelector("#modal__button-queue").textContent =
+      "Remove from queue";
+  } else {
+    document.querySelector("#modal__button-queue").textContent = "Add to queue";
+  }
+}
