8821f70d726e811a2b89a1bb3f1040ed7bfc5067
KlaudiaKrekora <klaudia.krekora22@gmail.com>
2023-05-27 14:36:57 +0200

    repair a mistake

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 import "../sass/main.scss";
 import { fetchMostPopular } from "./most-popular";
+import "./search-box";
 import { toggleWatched, toggleQueue } from "./modal";
 import { showSpinner, hideSpinner } from "./loading-spinner";
