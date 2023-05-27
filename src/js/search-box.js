e97a1d1717400ec12a8b0fa32334d5c974b45eaf
KlaudiaKrekora <klaudia.krekora22@gmail.com>
2023-05-27 17:56:03 +0200

    Update search-box.js

--- src/js/search-box.js
+++ src/js/search-box.js
@@ -33,30 +33,26 @@ function showMovies(movies) {
     movieEl.classList.add("card");
 
     movieEl.innerHTML = `
-    <div id="card" class="card">
-             <img class="card__poster" src="${IMG_URL}${
-      movie.poster_path
-    }" alt="${movie.original_title}" title="${movie.original_title}" />
-           <div class="card__content">
-                <div class="card__info">
-                  <div class="card__title">${movie.original_title}</div>
-                  <div class="card__genre">${movie.genres} |</div>
-                  <div class="card__release">${movie.release_date.slice(
-                    0,
-                    4
-                  )} |</div>
-                  <div class="card__rating">Rating: ${movie.vote_average}</div>
-                </div>
-              </div>
-            </div>
-          `;
+      <div id="card" class="card">
+        <img class="card__poster" src="${IMG_URL}${movie.poster_path}" alt="${
+      movie.original_title
+    }" title="${movie.original_title}" />
+        <div class="card__content">
+          <div class="card__info">
+            <div class="card__title">${movie.original_title}</div>
+            <div class="card__genre">${movie.genres} |</div>
+            <div class="card__release">${movie.release_date.slice(0, 4)} |</div>
+            <div class="card__rating">Rating: ${movie.vote_average}</div>
+          </div>
+        </div>
+      </div>`;
 
     moviesContainerEl.appendChild(movieEl);
   });
 }
 
-form.addEventListener("submit", e => {
-  e.preventDefault();
+form.addEventListener("submit", event => {
+  event.preventDefault();
 
   const searchTerm = search.value;
 
